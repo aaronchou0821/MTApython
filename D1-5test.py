718850055fb22f70971ab8144cd526682861b08d
aaronchou0821 <78331221+aaronchou0821@users.noreply.github.com>
2021-02-01 17:09:22 +0800

    Add files via upload

---
+++ D1-5test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:12:53 2021
+
+@author: TQC
+"""
+
+n=1
+total=10
+while n<=10:
+    total += n
+    n+=1
+print (total)
---
+++ D1-6test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:35:32 2021
+
+@author: TQC
+"""
+A=2
+for n in range(1,2):
+    A=A*(n+1)
+print(A)
+
+
+     
